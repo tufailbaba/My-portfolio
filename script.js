66f571052d3ba66aa9a20e4a717e54d0a3536f3c
tufailbaba <tufailbabacoc@gmail.com>
2025-07-23 17:52:55 +0000

    update1.1

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var typed = new Typed('#element', {
-    strings: ['Aspiring web developer', 'Video editor', 'Cinematographer', 'Cricketer'],
-    typeSpeed: 50,
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('#element', {
+        strings: ['Aspiring web developer', 'Video editor', 'Cinematographer', 'Cricketer'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
 });
